1e0b819c52ed21882a5e9f220ba7b621859e3f12
velltechgroup <velltechgroup@gmail.com>
2023-12-14 20:46:37 +0300

    aistoryteller

---
+++ client/next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  devIndicators: {
+    port: 4000,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      // {
+      //   protocol: "https",
+      //   hostname: "localhost",
+      // },
+      {
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
---
+++ client/src/utils/Spinner.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Spinner = () => {
+  return (
+    <div className="loadingSpinnerContainer">
+      <div className="loadingSpinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
