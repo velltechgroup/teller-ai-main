87d18416f7ace4a4d3485f3f2875a35ecda13fb3
velltechgroup <velltechgroup@gmail.com>
2023-12-19 09:07:59 +0300

    remake url

--- client/src/interceptor/http.ts
+++ client/src/interceptor/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'https://mystery-api-f87n.onrender.com/api/',
+    baseURL: 'https://mystery-api-f87n.onrender.com/api',
     withCredentials: true
 });
 
