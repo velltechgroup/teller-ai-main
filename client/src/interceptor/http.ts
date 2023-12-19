c68b5750f32a0cc509d9298b0ad2e9c11b941aea
velltechgroup <velltechgroup@gmail.com>
2023-12-19 08:52:00 +0300

    api endpoint

--- client/src/interceptor/http.ts
+++ client/src/interceptor/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://mystery-api-f87n.onrender.com/api/',
     withCredentials: true
 });
 
